7042cad78b8937a47445c0ee91a499fc84cda112
must <must@gmail.com>
2023-03-07 19:44:11 +0400

    norm finished

--- parsing/ms_dollar_han.c
+++ parsing/ms_dollar_han.c
@@ -6,31 +6,11 @@
 /*   By: mradwan <mradwan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 17:11:33 by mradwan           #+#    #+#             */
-/*   Updated: 2023/03/07 19:41:37 by mradwan          ###   ########.fr       */
+/*   Updated: 2023/03/07 19:43:41 by mradwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-// static char	*storing(char *str, int start, int len, const char *replace)
-// {
-// 	int		replace_len;
-// 	int		str_len;
-// 	int		new_len;
-// 	char	*new_str;
-
-// 	replace_len = ft_strlen(replace);
-// 	str_len = ft_strlen(str);
-// 	new_len = str_len - len + replace_len;
-// 	new_str = malloc(new_len + 1);
-// 	if (!new_str || !str)
-// 		return (NULL);
-// 	ft_strlcpy(new_str, str, start + 1);
-// 	ft_strlcat(new_str, replace, new_len + 1);
-// 	ft_strlcat(new_str, &str[start + len], new_len + 1);
-// 	free(str);
-// 	return (new_str);
-// }
 
 char	*storing(char *str, int start, int len, char *replace)
 {
@@ -144,61 +124,14 @@ void	dollar_expansion(char **str)
 	var.in_quotes = 0;
 	while ((*str)[var.i])
 	{
-		// if ((*str)[var.i] == '\"')
-		// {
-		// 	if (!var.in_d_quotes)
-		// 		var.in_d_quotes = 1;
-		// 	else if (var.in_d_quotes)
-		// 		var.in_d_quotes = 0;
-		// }
-		// if ((*str)[var.i] == '\'' && !var.in_d_quotes)
-		// {
-		// 	if (!var.in_quotes)
-		// 		var.in_quotes = 1;
-		// 	else if (var.in_quotes)
-		// 		var.in_quotes = 0;
-		// }
 		quotes_check(str, &var);
 		if ((*str)[var.i] == '$' && !var.in_quotes && (*str)[var.i + 1])
 		{
-			// var.i++;
-			// if ((*str)[var.i] == '?' && var.i == 1 && (*str)[var.i - 1] == '$')
-			// {
-			// 	expanded = ft_itoa(g_exit_code);
-			// 	*str = storing(*str, var.i - 1, 2, expanded);
-			// 	var.i += ft_strlen(expanded) - 2;
-			// 	free(expanded);
-			// 	continue ;
-			// }
-			// if (!ft_isalpha((*str)[var.i]) && (*str)[var.i] != '_')
-			// 	continue ;
-			// var.len = 0;
-			// while (ft_isalnum((*str)[var.i]) || (*str)[var.i] == '_')
-			// {
-			// 	var.len++;
-			// 	var.i++;
-			// }
 			if (expand(str, expanded, &var))
 				continue ;
 			tmp = ft_substr(*str, var.i - var.len, var.len);
 			if (generate_string(str, expanded, &tmp, &var))
 				continue ;
-			// if (tmp)
-			// {
-			// 	expanded = getenv(tmp);
-			// 	if (expanded)
-			// 	{
-			// 		*str = storing(*str, var.i - var.len - 1, var.len + 1, expanded);
-			// 		var.i += ft_strlen(expanded) - var.len - 1;
-			// 	}
-			// 	else if (!expanded)
-			// 	{
-			// 		*str = storing(*str, var.i - var.len - 1, var.len + 1, "");
-			// 		var.i -= var.len + 1;
-			// 	}
-			// 	free(tmp);
-			// 	continue ;
-			// }
 		}
 		var.i++;
 	}
