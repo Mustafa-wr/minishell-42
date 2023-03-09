eda45a6e26ceb05a087cde45c4bcfd35adb4646a
tukka <aboodytukka@gmail.com>
2023-03-09 06:05:47 +0400

    2 && 3 && 4 are done && program stucking on the last command after exec

--- ms_multiple_pipe.c
+++ ms_multiple_pipe.c
@@ -6,7 +6,7 @@
 /*   By: abdamoha <abdamoha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 16:00:16 by abdamoha          #+#    #+#             */
-/*   Updated: 2023/03/08 23:30:27 by abdamoha         ###   ########.fr       */
+/*   Updated: 2023/03/09 05:56:15 by abdamoha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ void	multiple_pipes(t_cmds *p, t_pipe *c)
 		i++;
 	}
 	i = 0;
-	printf("cmdlen = %d\n", p->cmd_len);
 	while (j < p->cmd_len)
 	{
 		c->pid = fork();
@@ -43,7 +42,6 @@ void	multiple_pipes(t_cmds *p, t_pipe *c)
 		{
 			if (j == 0)
 			{
-				printf("first\n");
 				dup2(fd[0][1], STDOUT_FILENO);
 				close(fd[0][1]);
 				close(fd[1][1]);
@@ -55,38 +53,30 @@ void	multiple_pipes(t_cmds *p, t_pipe *c)
 					printf("error_execve");
 					exit(0);
 				}
-				// free(cmd_exec);
 			}
 			else if (j == p->cmd_len - 1)
 			{
-				printf("last");
 				printf("i in = %d\n", i);
 				if (i == 0)
 				{
-					printf("1\n");
+					// printf("1\n");
 					dup2(fd[i + 1][i], STDIN_FILENO);
 				}
 				else if (j == 1)
-				{
-					printf("2\n");
 					dup2(fd[i - 1][i - 1], STDIN_FILENO);
-				}
 				else
-				{
-					printf("3\n");
 					dup2(fd[i - 1][i - 1], STDIN_FILENO);
-				}
 				close(fd[0][1]);
 				close(fd[1][1]);
 				close(fd[1][0]);
 				close(fd[0][0]);
 				cmd_exec = check_command_existence(p[j].cmd[0], c->m_path);
-				// printf("cmd = %s\n", cmd_exec);
 				if (execve(cmd_exec, p[j].cmd, NULL) < 0)
 				{
 					printf("error_execve");
 					exit(0);
 				}
+				// exit(0);
 			}
 			else
 			{
@@ -122,21 +112,26 @@ void	multiple_pipes(t_cmds *p, t_pipe *c)
 			close(fd[0][1]);
 			close(fd[0][0]);
 			pipe(fd[0]);
+			if (fd[0][0] < 0 || fd[0][1] < 0)
+			{
+				printf("error pipe\n");
+				exit(1);
+			}
 			i = 0;
 		}
 		else
 			i++;
-		printf("i = %d\n", i);
-		// while (k < p->cmd_len)
-		// {
-		// 	wait(NULL);
-		// 	k++;
-		// }
 		// close(fd[0][0]);
 		// close(fd[0][1]);
+		// close(fd[1][0]);
+		// close(fd[1][1]);
+		// close(fd[0][1]);
+		// close(fd[0][0]);
 		// pipe(fd[0]);
+		// close(fd[0][1]);
+		// close(fd[0][0]);
+		printf("i = %d\n", i);
 		j++;
-		// i++;
 	}
 	k = 0;
 	close(fd[0][0]);
@@ -148,87 +143,4 @@ void	multiple_pipes(t_cmds *p, t_pipe *c)
 		wait(NULL);
 		k++;
 	}
-	// waitpid(pid, NULL, 0);
-	// waitpid(pid2, NULL, 0);
-	// waitpid(pid3, NULL, 0);
-	// waitpid(pid4, NULL, 0);
-	// wait(NULL);
-	// wait(NULL);
-	// wait(NULL);
-	// wait(NULL);
-	// int pid = fork();
-	// if (pid == 0)
-	// {
-	// 	dup2(fd[0][1], STDOUT_FILENO);
-	// 	close(fd[0][1]);
-	// 	close(fd[1][1]);
-	// 	close(fd[1][0]);
-	// 	close(fd[0][0]);
-	// 	cmd_exec = check_command_existence(p[i].cmd[0], c->m_path);
-	// 	if (execve(cmd_exec, p[i].cmd, NULL) < 0)
-	// 	{
-	// 		printf("error_execve");
-	// 		exit(0);
-	// 	}
-	// 	free(cmd_exec);
-	// }
-	// i++;
-	// int	pid2 = fork();
-	// if (pid2 == 0)
-	// {
-	// 	dup2(fd[0][0], STDIN_FILENO);
-	// 	dup2(fd[1][1], STDOUT_FILENO);
-	// 	close(fd[0][1]);
-	// 	close(fd[1][1]);
-	// 	close(fd[1][0]);
-	// 	close(fd[0][0]);
-	// 	cmd_exec = check_command_existence(p[i].cmd[0], c->m_path);
-	// 	if (execve(cmd_exec, p[i].cmd, NULL) < 0)
-	// 	{
-	// 		printf("error_execve");
-	// 		exit(0);
-	// 	}
-	// }
-	// i++;
-	// close(fd[0][0]);
-	// close(fd[0][1]);
-	// pipe(fd[0]);
-	// if (fd[0][1] < 0 || fd[0][1] < 0)
-	// {
-	// 	printf("error");
-	// 	exit(0);
-	// }
-	// int	pid3 = fork();
-	// if (pid3 == 0)
-	// {
-	// 	dup2(fd[1][0], STDIN_FILENO);
-	// 	dup2(fd[0][1], STDOUT_FILENO);
-	// 	close(fd[0][1]);
-	// 	close(fd[1][1]);
-	// 	close(fd[1][0]);
-	// 	close(fd[0][0]);
-	// 	cmd_exec = check_command_existence(p[i].cmd[0], c->m_path);
-	// 	if (execve(cmd_exec, p[i].cmd, NULL) < 0)
-	// 	{
-	// 		printf("error_execve");
-	// 		exit(0);
-	// 	}
-	// }
-	// i++;
-	// int	pid4 = fork();
-	// if (pid4 == 0)
-	// {
-	// 	dup2(fd[0][0], STDIN_FILENO);
-	// 	close(fd[0][1]);
-	// 	close(fd[1][1]);
-	// 	close(fd[1][0]);
-	// 	close(fd[0][0]);
-	// 	cmd_exec = check_command_existence(p[i].cmd[0], c->m_path);
-	// 	if (execve(cmd_exec, p[i].cmd, NULL) < 0)
-	// 	{
-	// 		printf("error_execve");
-	// 		exit(0);
-	// 	}
-	// }
-	// exit(0);
 }
